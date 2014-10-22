d6540c424a8f72b3c11e17c12bb8a0d87d6d875b
Justin Oroz <Justin@justins-mbp.wireless.sbcc.local>
2014-10-22 16:41:07 -0700

    Initial Commit

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Minesweeper
+//
+//  Created by Justin Oroz on 10/22/14.
+//  Copyright (c) 2014 jmoroz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
